b01a788227a34225315c01e1b9d6c1fb305647ed
Drebezg <ruslanbox1@hotmail.com>
2017-08-13 16:35:50 +0300

    create uncontrolled componet

--- js/components/TestInput.js
+++ js/components/TestInput.js
@@ -1,26 +1,27 @@
 class TestInput extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {myValue: ''};
-		this.onChangeHandler = this.onChangeHandler.bind(this);
+		//this.state = {myValue: ''};
+		//this.onChangeHandler = this.onChangeHandler.bind(this);
 		this.onBtnClickHandler = this.onBtnClickHandler.bind(this);
 	}
-	onChangeHandler = (e) => {
+	/*onChangeHandler = (e) => {
 		this.setState({myValue: e.target.value});
-	};
+	};*/
 	onBtnClickHandler = () => {
-		alert(this.state.myValue);
+		alert(ReactDOM.findDOMNode(this.refs.myTestInput).value);
+		console.log(this.refs);
 	}
 	render() {
 		return (
 			<div>
 				<input 
 					className="test-input" 
-					value={this.state.myValue}
-					onChange={this.onChangeHandler}
+					defaultValue=''
 					placeholder="insert value" 
+					ref='myTestInput'
 				/>
-				<button onClick={this.onBtnClickHandler}> ShowAlert</button>
+				<button onClick={this.onBtnClickHandler} ref="alert_button"> ShowAlert</button>
 			</div>	
 		);
 	}
